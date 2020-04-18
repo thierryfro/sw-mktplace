45be8fb00373164911278b68a9926854ccf81c6c
Bruno Tostes <btfjulio@hotmail.com>
2020-04-18 19:35:23 -0300

    add jquery to js and save jstag in lay app

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -6,6 +6,6 @@ class Offer < ApplicationRecord
 
   validates :stock, :price, :active, presence: true
 
-  accepts_nested_attributes_for :offer_products
+  accepts_nested_attributes_for :offer_products, reject_if: :all_blank, allow_destroy: true
 
 end
