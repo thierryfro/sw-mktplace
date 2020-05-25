02400f2c84c131e105699e89bc4f5ed1e7cc7d29
Bruno Tostes <btfjulio@hotmail.com>
2020-05-25 12:26:40 -0300

    comment methods

--- app/models/chart_offer.rb
+++ app/models/chart_offer.rb
@@ -26,21 +26,25 @@ class ChartOffer < ApplicationRecord
         'freight_weights.min_weight <= ? AND freight_weights.max_weight >= ?',
         weight, weight
       )
-      .where('zip_code_zones.start_zip_code <= ? AND zip_code_zones.end_zip_code >= ?','04900000', '04900000')
+      .where(
+        'zip_code_zones.start_zip_code <= ? AND zip_code_zones.end_zip_code >= ?',
+        '04900000', '04900000'
+      )
       .first
   end
 
   def calculate_freight
+    # sum all chart products parsed_weights
     total_weight = chart.chart_offers.reduce(0) do |total, current_offer|
       offer_weight = current_offer.products.sum(&:parsed_weight)
       total + (offer_weight * current_offer.quantity)
     end
+    # find freight rule to match with weight X zip_code
     freight_rule = find_freight_rule(total_weight)
     if freight_rule
       chart.update!(freight_rule_id: freight_rule.id)
     else
       chart.update!(freight_rule_id: nil)
-      # 'Essa loja não entrega na sua região'
     end
   end
 end
