e5ea9199d6deffff0177198826a8898a86cb07a3
Bruno Tostes <btfjulio@hotmail.com>
2020-05-28 11:38:49 -0300

    change freight logic to chart model

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Chart < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :freight_rule, optional: true
@@ -6,9 +8,34 @@ class Chart < ApplicationRecord
   def total_weight
     return 0 if chart_offers.empty?
 
+    # sum all chart_offers products parsed_weights from chart
     chart_offers.reduce(0) do |total, current_offer|
       offer_weight = current_offer.products.sum(&:parsed_weight)
       total + (offer_weight * current_offer.quantity)
     end
+  end
+
+  def update_freight
+    if chart_offers.empty?
+      update!(freight_rule_id: nil)
+      return 'Seu carrinho está vazio'
+    end
+
+    store = chart_offers.first.store
+    # given a zip code find store rules
+    zone_rules = store.find_zone_rules('04900000')
+    if zone_rules.blank?
+      update!(freight_rule_id: nil)
+      return 'Essa loja não entrega na sua região'
+    end
+    # given a weight code find store rules
+    weight_rules = store.find_weight_rules(total_weight)
+    if weight_rules.blank?
+      update!(freight_rule_id: nil)
+      return 'Seu carrinho excedeu o limite de peso'
+    end
+    # intersection between two selections
+    freight_rule = (zone_rules & weight_rules).first
+    update!(freight_rule_id: freight_rule.id)
   end
 end
--- app/models/chart_offer.rb
+++ app/models/chart_offer.rb
@@ -6,7 +6,10 @@ class ChartOffer < ApplicationRecord
 
   validates :quantity, presence: true
 
-  after_save :calculate_freight
+  # 
+  after_save do
+    chart.update_freight
+  end
 
   def products
     offer.products
@@ -14,33 +17,5 @@ class ChartOffer < ApplicationRecord
 
   def store
     offer.store
-  end
-
-  def find_freight_rule
-    weight = chart.total_weight
-    store_freight_rules = store.freight_rules
-    store_freight_rules
-      .joins(:freight_weight)
-      .joins(:zip_code_zones)
-      .where(
-        'freight_weights.min_weight <= ? AND freight_weights.max_weight >= ?',
-        weight, weight
-      )
-      .where(
-        'zip_code_zones.start_zip_code <= ? AND zip_code_zones.end_zip_code >= ?',
-        '04900000', '04900000'
-      )
-      .first
-  end
-
-  def calculate_freight
-    return chart.update!(freight_rule_id: nil) if chart.chart_offers.empty?
-    # find freight rule to match with weight X zip_code
-    freight_rule = find_freight_rule
-    if freight_rule
-      chart.update!(freight_rule_id: freight_rule.id)
-    else
-      chart.update!(freight_rule_id: nil)
-    end
   end
 end
