b6825087106ccd834a746ca7fa9b369683b74677
Bruno Tostes <btfjulio@hotmail.com>
2020-06-12 09:09:33 -0300

    change seed to have no kits

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,10 +11,14 @@ class CartsController < ApplicationController
       @cart = Cart.find(current_user.cart_id)
       @cart_offers = @cart.cart_offers.order(created_at: :desc)
     else
+      @cart = Cart.find(session[:cart_id])
       @cart_offers = CartOffer.where(cart_id: session[:cart_id]).order(created_at: :desc)
     end
     @freight = @cart.freight_rule
-    @subtotal = @cart.calc_subtotal if @cart_offers
+    if @cart_offers
+      @subtotal = @cart.calc_subtotal
+      @sugested_offers = Offer.where(store: @cart.cart_store).sample(4)
+    end
   end
 
   # def session_offers(cart_offers)
@@ -38,10 +42,10 @@ class CartsController < ApplicationController
     # Cria um objeto de preferência
     items = []
     @cart.cart_offers.each do |cart_offer|
-      items << { 
-        title: cart_offer.offer.id, 
-        unit_price: cart_offer.offer.price, 
-        quantity: cart_offer.quantity 
+      items << {
+        title: cart_offer.offer.id,
+        unit_price: cart_offer.offer.price,
+        quantity: cart_offer.quantity
       }
     end
     preference_data = {
@@ -52,5 +56,4 @@ class CartsController < ApplicationController
     # Este valor substituirá a string "<%= @preference_id %>" no seu HTML
     @preference_id = preference['response']['id']
   end
-
 end
