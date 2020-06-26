957029ffe92ee259cca1792f3b65c1f26755c0f1
Bruno Tostes <btfjulio@hotmail.com>
2020-06-26 15:20:00 -0300

    add query to search bar

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,19 @@
-class AdminController < ApplicationController
+# frozen_string_literal: true
 
+class AdminController < ApplicationController
   # before_action :set_offer, only: [:show, :edit, :update, :destroy]
 
-  layout "admin_layout"
+  layout 'admin_layout'
 
-  def dashboard
-  end
+  def dashboard; end
 
   def offers
     @store = Store.find_by(owner_id: current_user.id)
-    @offers = Offer.where(store: @store).order(:price).page params[:page]
+
+    @offers = Offer.includes(:products).where(store: @store).order(:price)
+    query = params[:admin_query]
+    search_offers(query) if query
+    @offers = @offers.page params[:page]
   end
 
   def new_offer
@@ -27,11 +31,15 @@ class AdminController < ApplicationController
   private
 
   def offer_params
-    params.require(:offer).permit(:store_id, :stock, :price, :active, offer_products_attributes: [:product_id, :_destroy])
+    params.require(:offer).permit(:store_id, :stock, :price, :active, offer_products_attributes: %i[product_id _destroy])
   end
 
   def store_params
     params.require(:store).permit(:name, :email, :cnpj, :comercial_name, :owner_id)
   end
 
+  def search_offers(query)
+    products = Product.search_products(query)
+    @offers = @offers.where(products: { id: products.pluck(:id) })
+  end
 end
