ec1ffea9410cffa3a876507bc195475c905d07bd
Bruno Tostes <btfjulio@hotmail.com>
2020-07-02 21:22:53 -0300

    add stores infos to offers data set

--- app/javascript/packs/controllers/modal_controller.js
+++ app/javascript/packs/controllers/modal_controller.js
@@ -2,16 +2,16 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
 
-    static targets = ['name', 'photo', 'price'];
+    static targets = ['name', 'photo', 'price', 'store'];
 
     setContent(data) {
         this.nameTarget.innerText = data.name;
         this.priceTarget.innerText = data.price;
         this.photoTarget.src = data.photo;
+        this.storeTarget.href = data.store;
     }
 
     open(data) {
-        debugger
         this.setContent(data)
         this.element.classList.add("open");
     }
--- app/javascript/packs/controllers/offers_controller.js
+++ app/javascript/packs/controllers/offers_controller.js
@@ -1,11 +1,14 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-    static targets = ["name", "brand", "price", "photo", "modal"]
+    static targets = ["name", "brand", "price", "photo", "modal", "store"]
 
 
     initModal() {
+        debugger    
         const data = {
+            store: this.storeTarget.dataset.link,
+            storeName: this.storeTarget.dataset.name,
             name: this.nameTarget.innerText,
             price: this.priceTarget.innerText,
             brand: this.brandTarget.innerText,
